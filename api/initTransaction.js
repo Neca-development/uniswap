23a0c437beed25b3ed354d7a7debd6bc2efd75e5
Nikita Schastnyy <smynay@yandex.ru>
2021-03-02 19:29:49 +0300

    Road to web3

--- api/initTransaction.js
+++ api/initTransaction.js
@@ -1,25 +1,28 @@
 const { Percent } = require('@uniswap/sdk');
 const { ethers } = require('ethers');
+const Web3 = require('web3');
 
 const [ getIntFromPercent ] = require('../utils');
 const getTrade = require('./getTrade');
 
 async function initTransaction(inputTokenB, inputCount, inputSlippage, inputDeadline){
-  const { tokenA, tokenB, trade } = await getTrade(inputTokenB, inputCount);
+  const web3 = new Web3(process.env.INFURA_WSS_ROPSTEN);
   const provider = require('../network/provider');
   const account = require('../network/account');
+  
+  const { tokenA, tokenB, trade } = await getTrade(inputTokenB, inputCount);
 
   // TRANSACTION VALUES
   const slippageTolerance = new Percent(getIntFromPercent(inputSlippage).toString(), '1000');
 
-  const amountOutMin = ethers.BigNumber.from(trade.minimumAmountOut(slippageTolerance).raw.toString()).toHexString();
+  const amountOutMin = web3.utils.numberToHex(trade.minimumAmountOut(slippageTolerance).raw.toString());
   const path = [tokenA.address, tokenB.address];
   const to = process.env.PUBLIC_KEY;
   const deadline = Math.floor(Date.now() / 1000) + 60 * inputDeadline;
-  const value = ethers.BigNumber.from(trade.inputAmount.raw.toString()).toHexString();
+  const value = web3.utils.numberToHex(trade.inputAmount.raw.toString());
 
-  const gasLimit = ethers.BigNumber.from('300000').toHexString();
-  const gasPrice = (await provider.getGasPrice()).toHexString();
+  const gasLimit = web3.utils.numberToHex('300000');
+  const gasPrice = web3.utils.numberToHex(await provider.getGasPrice());
 
   // CONTRACT INIT (we may add other contracts)
   const uniswap = new ethers.Contract(process.env.ROUTER_ADDRESS, [
