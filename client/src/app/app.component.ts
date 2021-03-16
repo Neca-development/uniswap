7023ee64c0ee842e5f38a35b5f234b119a0b8579
Nikita Schastnyy <smynay@yandex.ru>
2021-03-16 11:08:06 +0300

    Add manual gasLimit support

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -222,7 +222,8 @@ export class AppComponent implements OnInit {
               this.settings.address,
               this.settings.privateKey,
               this.settings.network.chainId,
-              !this.swap.gasVariant ? 0 : +this.swap.gasPrice
+              !this.swap.gasVariant ? 0 : +this.swap.gasPrice,
+              !this.swap.gasVariant ? '300000' : this.swap.gasLimit
             );
             console.log(receipt);
             this.data.status = `
