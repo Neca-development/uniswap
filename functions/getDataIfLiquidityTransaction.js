9f9272ecd63b024703c315c5eda4c379bb4723ba
Nikita Schastnyy <smynay@yandex.ru>
2021-03-22 15:14:49 +0300

    Fixes

--- functions/getDataIfLiquidityTransaction.js
+++ functions/getDataIfLiquidityTransaction.js
@@ -8,7 +8,7 @@ async function getDataIfLiquidityTransaction(nodeAddress, transactionHash){
 
   const transaction = await web3.eth.getTransaction(transactionHash);
   
-  if(transaction.to == '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'){
+  if(transaction?.to == '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'){
     const decodedData = decoder.decodeData(transaction.input);
 
     if (decodedData.method == 'addLiquidityETH'){
