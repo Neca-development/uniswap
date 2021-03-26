cd010a9d570b201315c2ae4a26aa8de640f6b5ee
Nikita Schastnyy <smynay@yandex.ru>
2021-03-26 15:00:58 +0300

    Small fixes

--- services/logger.js
+++ services/logger.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const format = require("node.date-time") // ??
+const format = require('node.date-time');
 
 class Logger {
   constructor(){
